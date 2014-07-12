1135605ae84a65eb7ea7025a79824e970b608912
wooorm <tituswormer@gmail.com>
2014-07-12 12:01:06 +0200

    Removed unused code

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
 'use strict';
 
-var direction, GROUP_LEFT_TO_RIGHT, GROUP_RIGHT_TO_LEFT,
-    EXPRESSION_LEFT_TO_RIGHT, EXPRESSION_RIGHT_TO_LEFT;
-
-direction = require('direction');
-
-GROUP_LEFT_TO_RIGHT = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6' +
-    '\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF\u200E\u2C00-\uFB1C' +
-    '\uFE00-\uFE6F\uFEFD-\uFFFF';
-
-GROUP_RIGHT_TO_LEFT = '\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC';
-
-EXPRESSION_LEFT_TO_RIGHT = new RegExp(
-    '^[^' + GROUP_RIGHT_TO_LEFT + ']*[' + GROUP_LEFT_TO_RIGHT + ']'
-);
-
-EXPRESSION_RIGHT_TO_LEFT = new RegExp(
-    '^[^' + GROUP_LEFT_TO_RIGHT + ']*[' + GROUP_RIGHT_TO_LEFT + ']'
-);
+var direction = require('direction');
 
 function onchangedirectioninside(parent) {
     if (!parent) {
