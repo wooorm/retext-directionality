844b80b729ea39e6e34d9857b14f988a00630352
wooorm <tituswormer@gmail.com>
2014-09-26 09:50:25 +0200

    Refactor spec
    Includes changes for retext@0.2.0

--- spec/retext-directionality.spec.js
+++ spec/retext-directionality.spec.js
@@ -1,104 +1,197 @@
 'use strict';
 
-var directionality = require('..'),
-    visit = require('retext-visit'),
-    Retext = require('retext'),
-    assert = require('assert');
+/**
+ * Dependencies.
+ */
+
+var directionality,
+    visit,
+    content,
+    Retext,
+    assert;
+
+directionality = require('..');
+visit = require('retext-visit');
+content = require('retext-content');
+Retext = require('retext');
+assert = require('assert');
+
+/**
+ * Fixtures.
+ */
+
+var sentence,
+    leftToRightWords,
+    rightToLeftWords;
+
+/**
+ * Base English sentence.
+ */
+
+sentence = 'A simple, english, sentence.';
+
+/**
+ * French, baguette!
+ */
+
+leftToRightWords = 'Un simple anglais phrase'.split(' ');
+
+/**
+ * Arabic.
+ *
+ * I'm no good at Aabic---but Google says this is the
+ * translation of the example. For our purpose validity
+ * however does not matter---we just need some
+ * right-to-left words.
+ */
+
+rightToLeftWords = 'أ الجملة الانجليزية بسيطة'.split(' ');
+
+/**
+ * Retext
+ */
+
+var retext;
+
+retext = new Retext()
+    .use(visit)
+    .use(content)
+    .use(directionality);
+
+/**
+ * Tests
+ */
 
 describe('directionality()', function () {
-    var tree, leftToRightWords, rightToLeftWords;
-
-    it('should be of type `function`', function () {
+    it('should be a `function`', function () {
         assert(typeof directionality === 'function');
     });
 
-    tree = new Retext()
-        .use(visit)
-        .use(directionality)
-        .parse('A simple, english, sentence.');
-
-    // French, baguette!
-    leftToRightWords = 'Un simple anglais phrase'.split(' ');
-
-    // I'm no good at arabic—but google says this is the translation of the
-    // example. For our purpose this however does not matter—we just need
-    // some right-to-left words
-    rightToLeftWords = 'أ الجملة الانجليزية بسيطة'.split(' ');
-
-    it('should detect the direction of each `WordNode`', function () {
-        tree.visitType(tree.WORD_NODE, function (wordNode) {
-            assert('direction' in wordNode.data);
-            assert(wordNode.data.direction === 'ltr');
-        });
-
-        var iterator = -1;
-
-        tree.visitType(tree.WORD_NODE, function (wordNode) {
-            wordNode[0].fromString(leftToRightWords[++iterator]);
-            assert(wordNode.data.direction === 'ltr');
-        });
+    it('should expose an `attach` method', function () {
+        assert(typeof directionality.attach === 'function');
     });
 
-    it('should set the direction of a `WordNode` to null, when no value ' +
-        'is given', function () {
-            tree.visitType(tree.WORD_NODE, function (wordNode) {
-                wordNode[0].fromString('');
-                assert(wordNode.data.direction === 'neutral');
+    retext.parse(sentence, function (err, tree) {
+        it('should not throw', function (done) {
+            done(err);
+        });
+
+        it('should process each `WordNode`', function () {
+            var index;
+
+            tree.visitType(tree.WORD_NODE, function (node) {
+                assert(node.data.direction === 'ltr');
             });
 
-            var iterator = -1;
+            /**
+             * Cover some optimizations: when a node's
+             * direction does not change, there is no
+             * need to re-process parents.
+             */
 
-            tree.visitType(tree.WORD_NODE, function (wordNode) {
-                wordNode[0].fromString(leftToRightWords[++iterator]);
-                assert(wordNode.data.direction === 'ltr');
+            index = -1;
+
+            tree.visitType(tree.WORD_NODE, function (node) {
+                node[0].fromString(leftToRightWords[++index]);
+
+                assert(node.data.direction === 'ltr');
+            });
+        });
+
+        it('should process each `Parent`', function () {
+            tree.visitType(tree.WORD_NODE, function (node) {
+                var parent;
+
+                parent = node.parent;
+
+                while (parent) {
+                    assert(parent.data.direction === 'ltr');
+
+                    parent = parent.parent;
+                }
+            });
+        });
+
+        it('should set each `direction` to `null` when a `WordNode` (no ' +
+            'longer?) has a value',
+            function () {
+                tree.visitType(tree.WORD_NODE, function (node) {
+                    /**
+                     * We use the less secure `fromString`
+                     * on a TextNode here to test for
+                     * `changetext` events.
+                     */
+
+                    node[0].fromString('');
+
+                    assert(node.data.direction === 'neutral');
+                });
+            }
+        );
+
+        it('should re-process each `WordNode` and `Parent`', function () {
+            var index;
+
+            /**
+             * First set some left-to-rigth words, so we
+             * can check for `neutral` changes.
+             */
+
+            index = -1;
+
+            tree.visitType(tree.WORD_NODE, function (node) {
+                node.replaceContent(leftToRightWords[++index]);
+
+                assert(node.data.direction === 'ltr');
+            });
+
+            index = -1;
+
+            tree.visitType(tree.WORD_NODE, function (node) {
+                var parent,
+                    shouldBeDirection;
+
+                node.replaceContent(rightToLeftWords[++index]);
+
+                assert(node.data.direction === 'rtl');
+
+                /**
+                 * When all words are `rtl`, the parent
+                 * should reflext this instead of showing
+                 * `neutral`.
+                 */
+
+                shouldBeDirection = index === 3 ? 'rtl' : 'neutral';
+
+                parent = node.parent;
+
+                while (parent) {
+                    assert(parent.data.direction === shouldBeDirection);
+
+                    parent = parent.parent;
+                }
+            });
+        });
+
+        it('should set `Parent`s `direction` to `neutral` when all its ' +
+            '`WordNode`s are removed',
+            function () {
+                var parent;
+
+                parent = tree.head.head;
+
+                tree.visitType(tree.TEXT_NODE, function (node) {
+                    if (node.parent.type === node.WORD_NODE) {
+                        node.remove();
+                    }
+                });
+
+                while (parent) {
+                    assert(parent.data.direction === 'neutral');
+
+                    parent = parent.parent;
+                }
             }
         );
     });
-
-    it('should detect the direction of each `Parent`', function () {
-        tree.visitType(tree.WORD_NODE, function (wordNode) {
-            var parent = wordNode;
-
-            while (parent.parent) {
-                parent = parent.parent;
-                assert('direction' in parent.data);
-                assert(parent.data.direction === 'ltr');
-            }
-        });
-    });
-
-    it('should automatically redetect `WordNode`s and their parents ' +
-        'when their value changes', function () {
-            var iterator = -1;
-
-            tree.visitType(tree.WORD_NODE, function (wordNode) {
-                var parent = wordNode, shouldBeDirection;
-
-                wordNode[0].fromString(rightToLeftWords[++iterator]);
-                assert(wordNode.data.direction === 'rtl');
-
-                shouldBeDirection = iterator === 3 ? 'rtl' : 'neutral';
-
-                while (parent.parent) {
-                    parent = parent.parent;
-                    assert(parent.data.direction === shouldBeDirection);
-                }
-            });
-        }
-    );
-
-    it('should automatically set previous parents direction to `"neutral"`' +
-        'when all `WordNode`s are removed', function () {
-            var parent = tree.head.head;
-
-            tree.visitType(tree.TEXT_NODE, function (node) {
-                node.remove();
-            });
-
-            while (parent) {
-                assert(parent.data.direction === 'neutral');
-                parent = parent.parent;
-            }
-        }
-    );
 });
